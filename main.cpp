57ef544a988edf5872e0e152a6bd05ad1182553d
Arkadij Chistyj <arkdchst@gmail.com>
2019-10-23 20:24:59 +0000

    all sf::VertexArray replaced by std::vector

--- main.cpp
+++ main.cpp
@@ -1,13 +1,69 @@
 #include <time.h>
 #include <stdio.h>
 #include <math.h>
+#include <vector>
 
 #include <SFML/Graphics.hpp>
 
 
-sf::VertexArray * initLines(){
+// std::vector<float> from_base(){
+
+// 	std::vector<float> a(2);
+// 	return std::vector<float> (2);
+// }
+
+
+std::vector<float> ** initLines(){
+
+	std::vector<float> **lines = new std::vector<float>*[2];
+		lines[0] = new std::vector<float>[2];
+			lines[0][0] = std::vector<float>(2, 0);
+			lines[0][1] = std::vector<float>(2, 0);
+		lines[1] = new std::vector<float>[2];
+			lines[1][0] = std::vector<float>(2, 0);
+			lines[1][1] = std::vector<float>(2, 0);
+
+	return lines;
+
+}
+
+
+void process(std::vector<float> **lines, float time, sf::RenderWindow &window){
+	const float v0 = 10;
+	const float l0 = 50;
+	float a = sin(time);
+	float b = cos(time);
+
+	float l = v0 * time;
+
+	float x1 = l * cos(a);
+	float y1 = l * sin(a);
+
+	float x2 = (l0 * sin(b) + l) * cos(a) - l0 * sin(a) * cos(b);
+	float y2 = (l0 * sin(b) + l) * sin(a) + l0 * cos(b) * cos(a);
+
+
+
+		lines[0][1][0] = x1;
+		lines[0][1][1] = y1;
+		lines[1][0][0] = x1;
+		lines[1][0][1] = y1;
+		lines[1][1][0] = x2;
+		lines[1][1][1] = y2;
+
+
+	printf("%f %f %f %f\n", x1, y1, x2, y2);
+}
+
+
+void draw(std::vector<float> **lines, sf::RenderWindow &window){
 	sf::VertexArray line1(sf::Lines, 2);
 	sf::VertexArray line2(sf::Lines, 2);
+
+		line1[0].position = sf::Vector2f(lines[0][0][0], lines[0][0][1]);
+		line1[1].position = sf::Vector2f(lines[0][1][0], lines[0][1][1]);
+		line2[0].position = sf::Vector2f(lines[1][0][0], lines[1][0][1]);
+		line2[1].position = sf::Vector2f(lines[1][1][0], lines[1][1][1]);
 
 		line1[0].color = sf::Color::Red;
 		line1[1].color = sf::Color::Red;
@@ -15,54 +71,20 @@ sf::VertexArray * initLines(){
 		line2[1].color = sf::Color::Green;
 
 
-	sf::VertexArray *lines = new sf::VertexArray[2];
-	lines[0] = line1;
-	lines[1] = line2;
-
-
-	return lines;
-
-}
-
-
-void process(sf::VertexArray * lines, float t, sf::RenderWindow &window){
-	const float v0 = 10;
-	const float l0 = 50;
-
-	float x1,y1,x2,y2;
-
-	x1 = v0 * t * cos(sin(t));
-	y1 = v0 * t * sin(sin(t));
-
-	x2 = x1 - l0 * sin(sin(t) - cos(t));
-	y2 = y1 + l0 * cos(sin(t) - cos(t));
-
-	lines[0][0].position = sf::Vector2f(0, 0);
-	lines[0][1].position = sf::Vector2f(x1, y1);
-	lines[1][0].position = sf::Vector2f(x1, y1);
-	lines[1][1].position = sf::Vector2f(x2, y2);
-
-
-	printf("%f %f %f %f\n", x1, y1, x2, y2);
-}
-
-
-void draw(sf::VertexArray * lines, sf::RenderWindow &window){
-
-	window.draw(lines[0]);
-	window.draw(lines[1]);
+	window.draw(line1);
+	window.draw(line2);
 }
 
 
 int main(){
-	float t = 0;
+	float time = 0;
 
 	sf::RenderWindow window(sf::VideoMode(600, 600), "", sf::Style::None);
 	window.display();
 
 	window.setView(sf::View(sf::Vector2f(250, 0), sf::Vector2f(500, -500)));
 
-	sf::VertexArray *lines = initLines();
+	std::vector<float> **lines = initLines();
 
 	while (window.isOpen()){
 		sf::Event event;
@@ -70,14 +92,14 @@ int main(){
 				window.close();
 
 		window.clear(sf::Color::Black);
-			process(lines, t, window);
+			process(lines, time, window);
 			draw(lines, window);
 			window.display();
 
 
-		sf::sleep(sf::milliseconds(10));
+		sf::sleep(sf::milliseconds(5));
 
-		t += 0.01;
+		time += 0.01;
 	}
 
 	return 0;
