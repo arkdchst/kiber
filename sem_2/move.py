7715140ce5ee73ad1d0336de6540eed31a428c59
Arkadij Chistyj <arkdchst@gmail.com>
2020-05-02 16:13:10 +0000

    two gaits implemented

--- sem_2/move.py
+++ sem_2/move.py
@@ -7,12 +7,6 @@ femur = 75
 tibia = 138
 
 
-'''
- 9  8  7 |2 5| 16 17 18
- 6  5  4 |1 4| 13 14 15
- 3  2  1 |0 3| 10 11 12
-'''
-
 num_to_ids = {0:(1,2,3), 2:(7,8,9), 3:(10,11,12), 5:(16,17,18)}
 middle_pos = {1: 300, 2: 461, 3:689, 7: 678, 8: 453, 9: 692, 10: 681, 11: 548, 12: 316, 16: 300, 17: 546, 18: 310}
 half_pi_pos = {1: -85, 2: 86, 3: 1071, 7: 315, 8: 87, 9: 1064, 10: 1048, 11: 922, 12: -63, 16: 689, 17: 920, 18: -62}
@@ -36,19 +30,19 @@ def get_angles(x, y, z):
 def step_fun(t):
 	return ((1 - cos(pi * t)) / 2, sin(pi * t))
 
+def back_fun(t):
+	return (1 - t, 0)
+
 
 
 class Leg:
 	num = None
-
 	xmin = None
 	ymin = None
 	xmax = None
 	ymax = None
-
 	z = None
-
-	def __init__(self, num, xmin=-50, ymin=-100, xmax=50, ymax=-50, z = coxa + femur + 50):
+	def __init__(self, num, xmin=-50, ymin=-100, xmax=50, ymax=-50, z = coxa + femur):
 		self.num = num
 		self.xmin = xmin
 		self.ymin = ymin
@@ -61,25 +55,100 @@ class Leg:
 			pos = angle_to_pos(angles[i], num_to_ids[self.num][i])
 			set_servo(num_to_ids[self.num][i], int(pos), int(time))
 	def set_point_norm(self, point, time):
-			point = (point[0] * (self.xmax - self.xmin) + self.xmin, point[1] * (self.ymax - self.ymin) + self.ymin)
-			self.set_point(point, self.z, time)
+		point = (point[0] * (self.xmax - self.xmin) + self.xmin, point[1] * (self.ymax - self.ymin) + self.ymin)
+		self.set_point(point, self.z, time)
 
 
 
 class Move:
 	leg = None
-
-	x1 = None
-	x2 = None
+	from_x = None
+	to_x = None
 	onAir = None
+	height = None
 	dt = None
+	interval = None
 	ready = False
-	def __init__(self, leg, x1, x2, onAir, dt, aSync = False):
+	t = 0
+	def __init__(self, leg, from_x, to_x, onAir, height, dt, interval, aSync = False):
 		self.leg = leg
-		self.x1 = x1
-		self.x2 = x2g
+		self.from_x = from_x
+		self.to_x = to_x
 		self.onAir = onAir
+		self.height = height
 		self.dt = dt
+		self.interval = interval
+	def tick(self):
+		if self.ready: return
+		self.t += self.dt
+		if self.t > 1: self.t = 1
+		if self.onAir:
+			point = step_fun(self.t)
+		else:
+			point = back_fun(self.t)
+		point = (point[0] * (self.to_x - self.from_x) + self.from_x, point[1] * self.height)
+		self.leg.set_point_norm(point, self.interval)
+		if self.t == 1: self.ready = True
 
-	def tick():
-		leg
+
+
+def move1():
+	leg0 = Leg(0, -100, -100, 0, -25)
+	leg2 = Leg(2, 0, -100, 100, -25)
+	leg3 = Leg(3, -100, -100, 0, -25)
+	leg5 = Leg(5, 0, -100, 100, -25)
+
+	t1 = 0.1
+	dt = 0.1
+
+	while True:
+		move=Move(leg0, 0, 1, True, 1, dt, t1 * 1000)
+		while not move.ready: move.tick(); sleep(t1)
+		move=Move(leg2, 0, 1, True, 1, dt, t1 * 1000)
+		while not move.ready: move.tick(); sleep(t1)
+		moves=[Move(leg0, 0.5, 1, False, 1, dt, t1 * 1000), Move(leg2, 0.5, 1, False, 1, dt, t1 * 1000), Move(leg3, 0, 0.5, False, 1, dt, t1 * 1000), Move(leg5, 0, 0.5, False, 1, dt, t1 * 1000)]
+		while not moves[0].ready:
+			for x in moves:
+				x.tick()
+			sleep(t1)
+		move=Move(leg3, 0, 1, True, 1, dt, t1 * 1000)
+		while not move.ready: move.tick(); sleep(t1)
+		move=Move(leg5, 0, 1, True, 1, dt, t1 * 1000)
+		while not move.ready: move.tick(); sleep(t1)
+		moves=[Move(leg0, 0, 0.5, False, 1, dt, t1 * 1000), Move(leg2, 0, 0.5, False, 1, dt, t1 * 1000), Move(leg3, 0.5, 1, False, 1, dt, t1 * 1000), Move(leg5, 0.5, 1, False, 1, dt, t1 * 1000)]
+		while not moves[0].ready:
+			for x in moves:
+				x.tick()
+			sleep(t1)
+
+
+def move2():
+	dt1 = 30
+	dt2 = 12
+	leg0 = Leg(0, -100, -100, 0, -25)
+	leg2 = Leg(2, -dt1, -100, 100-dt1, -25)
+	leg3 = Leg(3, -100, -100, 0, -25)
+	leg5 = Leg(5, -dt2, -100, 100-dt2, -25)
+
+	t1 = 0.4
+	dt = 0.3
+	t2 = 0.3
+	dt2 = 0.4
+
+	while True:
+		move1=Move(leg2, 0, 1, True, 1, dt, t1 * 1000)
+		move2=Move(leg3, 0, 1, True, 1, dt, t1 * 1000)
+		while not move1.ready: move1.tick(); move2.tick(); sleep(t1)
+		moves=[Move(leg0, 0, 0.5, False, 1, dt2, t2 * 1000), Move(leg2, 0.5, 1, False, 1, dt2, t2 * 1000), Move(leg3, 0.5, 1, False, 1, dt2, t2 * 1000), Move(leg5, 0, 0.5, False, 1, dt2, t2 * 1000)]
+		while not moves[0].ready:
+			for x in moves:
+				x.tick()
+			sleep(t2)
+		move1=Move(leg0, 0, 1, True, 1, dt, t1 * 1000)
+		move2=Move(leg5, 0, 1, True, 1, dt, t1 * 1000)
+		while not move1.ready: move1.tick(); move2.tick(); sleep(t1)
+		moves=[Move(leg0, 0.5, 1, False, 1, dt2, t2 * 1000), Move(leg2, 0, 0.5, False, 1, dt2, t2 * 1000), Move(leg3, 0, 0.5, False, 1, dt2, t2 * 1000), Move(leg5, 0.5, 1, False, 1, dt2, t2 * 1000)]
+		while not moves[0].ready:
+			for x in moves:
+				x.tick()
+			sleep(t2)
